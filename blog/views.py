5778dc9cc94eef1f1a2fa047c852ae772691db8a
Vladislav <vlad_novikov1999_2@mail.ru>
2020-09-25 16:44:38 +0300

    Заменил функцию post_list в '/blog/views.py' на класс PostListView(LastView)

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,29 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from .models import Post
 
 
-def post_list(request):
-    object_list = Post.published.all()
-    paginator = Paginator(object_list, 3)       # По 3 статьи на каждой странице
-    page = request.GET.get('page')
-    try:
-        posts = paginator.page(page)
-    except PageNotAnInteger:
-        # Если страница не является целым числом, возвращаем первую страницу
-        posts = paginator.page(1)
-    except EmptyPage:
-        # Если номер страницы больше, чем общее кол-во страни, возвращаем последнюю страницу
-        posts = paginator.page(paginator.num_pages)
-    return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
+class PostListView(ListView):       # Аналог функции post_list
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3     # Кол-во статей на странице
+    template_name = 'blog/post/list.html'
+
+
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 3)       # По 3 статьи на каждой странице
+#     page = request.GET.get('page')
+#     try:
+#         posts = paginator.page(page)
+#     except PageNotAnInteger:
+#         # Если страница не является целым числом, возвращаем первую страницу
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # Если номер страницы больше, чем общее кол-во страни, возвращаем последнюю страницу
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'page': page, 'posts': posts})
 
 
 def post_detail(request, day, month, year, post):
