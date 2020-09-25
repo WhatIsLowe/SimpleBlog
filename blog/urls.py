c74920ce64966ed783f4a651b2617d2627819b78
Vladislav <vlad_novikov1999_2@mail.ru>
2020-09-25 17:37:41 +0300

    some little changes

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     # Обработчик приложения блога
     # path('', views.post_list, name='post_list'),
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',       # Шаблон адреса
          views.post_detail, name='post_detail'),
 ]
