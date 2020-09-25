f9b6b2ae83f8edb448f70369bdea497f7d3bbcb9
Vladislav <vlad_novikov1999_2@mail.ru>
2020-09-25 16:08:35 +0300

    Первая верся сайта

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')     # отображение в списке статей
+    list_filter = ('status', 'created', 'publish', 'author')        # фильтарция статей по указанным полям
+    search_fields = ('title', 'body')       # Строка поиска (ведет поиск по указанным полям)
+    prepopulated_fields = {'slug': ('title',)}      # автоматически генерирует адр.строку (slug)
+    raw_id_fields = ('author',)     # поиск по авторам
+    date_hierarchy = 'publish'      # навигация по датам (по "publish")
+    ordering = ('status', 'publish')        # сортировка статей по-умолчанию
