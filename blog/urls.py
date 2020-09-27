706b5d1bdbfe867df5c2c8a8f1a065b139ecd2bf
Vladislav <vlad_novikov1999_2@mail.ru>
2020-09-27 18:15:49 +0300

    * Поменял СУБД с sqlite на postgre * Добавлен поиск по статьям

--- blog/urls.py
+++ blog/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('<int:post_id>/share/', views.post_share, name='post_share'),       # Шаблон для отправки почты
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),      # Шаблон для тэгов
     path('feed/', LatestPostFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),     # Шаблон поиска
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery,\
+    SearchRank, TrigramSimilarity
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404, HttpResponsePermanentRedirect
 from django.views.generic import ListView
 from taggit.models import Tag
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post, Comment
 
 
@@ -92,3 +94,25 @@ def post_share(request, post_id):
         form = EmailPostForm()
     return render(request, 'blog/post/share.html',
                       {'post': post, 'form': form, 'sent': sent})
+
+
+# Поиск по статьям
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            # search_vector = SearchVector('title', 'body')
+            # search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            # search_query = SearchQuery(query)
+            results = Post.objects.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
+    return render(request, 'blog/post/search.html', {
+        'form': form,
+        'query': query,
+        'results': results
+    })
